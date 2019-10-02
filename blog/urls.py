0e74a5797898940468cfc1d26eae2e99944633d3
Christopher Weilacker <chrisweilacker@gmail.com>
2019-10-02 12:21:55 -0700

    Added CSS to Blog Posts

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name="post_detail"),
+    path('<str:title>/', views.post_detail_by_title, name="post_detail_by_title")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     postList = {'posts': posts}
     return render(request, 'blog/post_list.html', postList)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    aPost = {'post': post}
+    return render(request, 'blog/post_detail.html', aPost)
+
+def post_detail_by_title(request, title):
+    post = get_object_or_404(Post, title=title)
+    aPost = {'post': post}
+    return render(request, 'blog/post_detail.html', aPost)
